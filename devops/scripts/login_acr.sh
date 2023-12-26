f1d85cc4e3e70c98345d54425e5ffdaadf79246f
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2023-12-26 02:24:20 +0000

    Add conditional check for exit code in login_acr.sh

--- devops/scripts/login_acr.sh
+++ devops/scripts/login_acr.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-set -o errexit
-set -o pipefail
-set -o nounset
-
 acr=$1
 
 az acr login --name "$acr"
-echo "##vso[task.setvariable variable=outcome;isoutput=true]success"
+
+lastExitCode=$?
+
+if [ $lastExitCode -eq 0 ]; then
+  echo "##vso[task.setvariable variable=outcome;isoutput=true]success"
+else
+  echo "##vso[task.setvariable variable=outcome;isoutput=true]failure"
+fi
+
+exit $lastExitCode
