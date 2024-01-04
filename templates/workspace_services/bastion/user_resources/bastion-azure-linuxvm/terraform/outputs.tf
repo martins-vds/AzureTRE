c7cbc67d758ce302ec52d8b68a69e84b78f2c9e4
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-04 23:00:03 +0000

    Update version and add TODOs for Shareable Link

--- templates/workspace_services/bastion/user_resources/bastion-azure-linuxvm/terraform/outputs.tf
+++ templates/workspace_services/bastion/user_resources/bastion-azure-linuxvm/terraform/outputs.tf
@@ -10,10 +10,6 @@ output "azure_resource_id" {
   value = azurerm_linux_virtual_machine.linuxvm.id
 }
 
-output "connection_uri" {
-  value = "https://${data.azurerm_linux_web_app.guacamole.default_hostname}/?/client/${textencodebase64("${azurerm_linux_virtual_machine.linuxvm.name}\u0000c\u0000azuretre", "UTF-8")}"
-}
-
 output "vm_username" {
   value = random_string.username.result
 }
