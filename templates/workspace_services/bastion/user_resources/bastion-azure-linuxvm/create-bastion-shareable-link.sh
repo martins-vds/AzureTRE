b73972707edf4095cbb734128069f536a08dc169
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-05 16:37:38 +0000

    Add functionality to create shareable link for Azure Linux VM in Bastion

--- templates/workspace_services/bastion/user_resources/bastion-azure-linuxvm/create-bastion-shareable-link.sh
+++ templates/workspace_services/bastion/user_resources/bastion-azure-linuxvm/create-bastion-shareable-link.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 set -o errexit
 set -o pipefail
 
@@ -23,4 +24,40 @@ request_body="$(cat <<EOF
 EOF
 )"
 
-az rest --method post --uri "$shareable_link_api_url" --body "$request_body" --output json
+get_access_token() {
+  az account get-access-token --output json | jq -r .accessToken
+}
+
+echo "Getting access token"
+access_token=$(get_access_token)
+echo "Creating shareable link"
+response=$(curl -si -X POST -H "Authorization: Bearer $access_token" -H "Content-Type: application/json" -d "$request_body" "$shareable_link_api_url")
+status_code=$(echo "$response" | head -n 1 | cut -d$' ' -f2)
+if [[ "$status_code" -eq 202 ]]; then
+  location=$(echo "$response" | grep -oP '[l|L]ocation: \K.*')
+  # Wait for the link to be created in a loop
+  while true; do
+    response=$(curl -si -H "Authorization: Bearer $access_token" -H "Content-Type: application/json" "$location")
+    status_code=$(echo "$response" | head -n 1 | cut -d$' ' -f2)
+    echo "Status code: $status_code"
+    if [[ "$response" == "" ]]; then
+      echo "Failed to check the status of the link creation"
+      exit 1
+    fi
+    status_code=$(echo "$response" | head -n 1 | cut -d$' ' -f2)
+    if [[ "$status_code" -eq 200 || "$status_code" -eq 202 ]]; then
+      echo "$response" | jq -r .value[0].bsl
+    elif [[ "$status_code" -eq 404 ]]; then
+      echo "$response" | jq -r .error.message
+    fi
+    sleep 5
+  done
+
+  # Get the link
+  echo "$response" | jq -r .value[0].bsl
+elif [[ "$status_code" -eq 200 ]]; then
+  echo "$response" | jq -r .value[0].bsl
+else
+  echo "Failed to create the shareable link"
+  exit 1
+fi
