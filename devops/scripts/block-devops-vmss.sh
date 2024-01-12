aef3ed0d08bf3614f98facad10d6a94c7791ab60
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-12 06:32:24 +0000

    Add VMSS resource group parameter to deploy_tre_reusable.yml

--- devops/scripts/block-devops-vmss.sh
+++ devops/scripts/block-devops-vmss.sh
@@ -4,9 +4,12 @@ set -e
 
 stg_rg=$1
 stg_account_name=$2
-vmss_vnet=$3
-vmss_subnet=$4
+vmss_rg=$3
+vmss_vnet=$4
+vmss_subnet=$5
 
 # Allow DevOps VMSS to access storage account
 
-az storage account network-rule remove -g "$stg_rg" --account-name "$stg_account_name" --vnet-name "$vmss_vnet" --subnet "$vmss_subnet"
+vmss_subnet_id=$(az network vnet subnet show -g "$vmss_rg" --vnet-name "$vmss_vnet" --name "$vmss_subnet" --query id -o tsv)
+
+az storage account network-rule remove -g "$stg_rg" --account-name "$stg_account_name" --subnet "$vmss_subnet_id"
