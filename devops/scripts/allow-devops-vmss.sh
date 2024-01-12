87dcfe6c1c46e9d34debdab2618e22435a238b26
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-12 07:17:27 +0000

    Update storage account access in VMSS scripts

--- devops/scripts/allow-devops-vmss.sh
+++ devops/scripts/allow-devops-vmss.sh
@@ -15,3 +15,4 @@ az network vnet subnet update -g "$vmss_rg" --vnet-name "$vmss_vnet" --name "$vm
 vmss_subnet_id=$(az network vnet subnet show -g "$vmss_rg" --vnet-name "$vmss_vnet" --name "$vmss_subnet" --query id -o tsv)
 
 az storage account network-rule add -g "$stg_rg" --account-name "$stg_account_name" --subnet "$vmss_subnet_id"
+az storage account update -g "$stg_rg" --name "$stg_account_name" --default-action Deny --public-network-access Enabled
--- devops/scripts/block-devops-vmss.sh
+++ devops/scripts/block-devops-vmss.sh
@@ -12,4 +12,5 @@ vmss_subnet=$5
 
 vmss_subnet_id=$(az network vnet subnet show -g "$vmss_rg" --vnet-name "$vmss_vnet" --name "$vmss_subnet" --query id -o tsv)
 
+az storage account update -g "$stg_rg" --name "$stg_account_name" --default-action Deny --public-network-access Disabled
 az storage account network-rule remove -g "$stg_rg" --account-name "$stg_account_name" --subnet "$vmss_subnet_id"
