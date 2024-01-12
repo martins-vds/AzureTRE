f95dffdbeaee39af9d14a5e2e43334836c37d51e
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-12 06:47:02 +0000

    Add service endpoint for DevOps VMSS to access storage account

--- devops/scripts/allow-devops-vmss.sh
+++ devops/scripts/allow-devops-vmss.sh
@@ -10,6 +10,12 @@ vmss_subnet=$5
 
 # Allow DevOps VMSS to access storage account
 
+location=$(az group show -g "$vmss_rg" --query location -o tsv)
+
+stg_service_endpoint=$(az network vnet list-endpoint-services -l "$location" --query "[?name == 'Microsoft.Storage'].id" -o tsv)
+
+az network vnet subnet update -g "$vmss_rg" --vnet-name "$vmss_vnet" --name "$vmss_subnet" --service-endpoints "$stg_service_endpoint"
+
 vmss_subnet_id=$(az network vnet subnet show -g "$vmss_rg" --vnet-name "$vmss_vnet" --name "$vmss_subnet" --query id -o tsv)
 
 az storage account network-rule add -g "$stg_rg" --account-name "$stg_account_name" --subnet "$vmss_subnet_id"
