2894b05142af60ffe3e8437ac7930c9ee36f9062
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-13 16:12:13 +0000

    Refactor storage account configuration

--- core/terraform/storage_network_rules.tf
+++ core/terraform/storage_network_rules.tf
@@ -1,6 +1,16 @@
-resource "azurerm_storage_account_network_rules" "network_rules" {
-  storage_account_id = data.azurerm_storage_account.mgmt_stg.id
+resource "azurerm_storage_account" "state_storage" {
+  name                            = var.mgmt_storage_account_name
+  resource_group_name             = azurerm_resource_group.mgmt.name
+  location                        = azurerm_resource_group.mgmt.location
+  account_tier                    = "Standard"
+  account_kind                    = "StorageV2"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
 
-  default_action = "Deny"
-  virtual_network_subnet_ids = [ module.network.resource_processor_subnet_id ]
+  network_rules {
+    default_action = "Deny"
+    virtual_network_subnet_ids = [ module.network.resource_processor_subnet_id ]
+  }
+
+  lifecycle { ignore_changes = [tags] }
 }
