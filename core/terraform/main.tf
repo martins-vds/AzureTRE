f7e62be187284ccb8d17793a1023b37c473d8d4f
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-13 17:14:04 +0000

    Remove azurerm_storage_account resource from main.tf and uncomment provisioner in network.tf

--- core/terraform/main.tf
+++ core/terraform/main.tf
@@ -185,20 +185,3 @@ module "terraform_azurerm_environment_configuration" {
   arm_environment = var.arm_environment
 }
 
-resource "azurerm_storage_account" "state_storage" {
-  name                            = var.mgmt_storage_account_name
-  resource_group_name             = azurerm_resource_group.mgmt.name
-  location                        = azurerm_resource_group.mgmt.location
-  account_tier                    = "Standard"
-  account_kind                    = "StorageV2"
-  account_replication_type        = "LRS"
-  allow_nested_items_to_be_public = false
-
-  network_rules {
-    default_action = "Deny"
-    virtual_network_subnet_ids = [ module.network.resource_processor_subnet_id ]
-  }
-
-  lifecycle { ignore_changes = [tags] }
-}
-
