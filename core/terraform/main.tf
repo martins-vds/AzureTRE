8c9191ea68585803236c06c055da5336d69e1664
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-13 16:57:43 +0000

    Remove storage network rules and add state storage resource

--- core/terraform/main.tf
+++ core/terraform/main.tf
@@ -184,3 +184,21 @@ module "terraform_azurerm_environment_configuration" {
   source          = "git::https://github.com/microsoft/terraform-azurerm-environment-configuration.git?ref=0.2.0"
   arm_environment = var.arm_environment
 }
+
+resource "azurerm_storage_account" "state_storage" {
+  name                            = var.mgmt_storage_account_name
+  resource_group_name             = azurerm_resource_group.mgmt.name
+  location                        = azurerm_resource_group.mgmt.location
+  account_tier                    = "Standard"
+  account_kind                    = "StorageV2"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
+
+  network_rules {
+    default_action = "Deny"
+    virtual_network_subnet_ids = [ module.network.resource_processor_subnet_id ]
+  }
+
+  lifecycle { ignore_changes = [tags] }
+}
+
