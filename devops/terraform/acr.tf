016b1d5a55ca34ac27665e057daec6897ea05c9c
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-15 04:08:15 +0000

    Update ACR configuration and version number

--- devops/terraform/acr.tf
+++ devops/terraform/acr.tf
@@ -48,7 +48,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "acrlink" {
   name                  = "acrmgmtlink"
   resource_group_name   = azurerm_resource_group.mgmt.name
   private_dns_zone_name = azurerm_private_dns_zone.azurecr.name
-  virtual_network_id    = regex("(?<virtual_network_id>.*)\\/subnets(.*)", var.mgmt_acr_private_endpoint_subnet_id).virtual_network_id
+  virtual_network_id    = regex("(?P<virtual_network_id>.*)\\/subnets\\/(?P<subnet>.*)", var.mgmt_acr_private_endpoint_subnet_id).virtual_network_id
   tags                  = azurerm_resource_group.mgmt.tags
   lifecycle { ignore_changes = [tags] }
 }
@@ -68,7 +68,7 @@ resource "azurerm_private_endpoint" "mgmt_acr_pe" {
 
   private_service_connection {
     name                           = "psc-acr-mgmt"
-    private_connection_resource_id = data.azurerm_container_registry.mgmt_acr.id
+    private_connection_resource_id = azurerm_container_registry.shared_acr.id
     is_manual_connection           = false
     subresource_names              = ["registry"]
   }
