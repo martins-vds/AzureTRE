18c1f574b26a0a3b856ebee99792bc6c0366d50f
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-17 23:11:30 +0000

    Remove validation for private_dns_zone_resource_group_name

--- core/terraform/variables.tf
+++ core/terraform/variables.tf
@@ -225,9 +225,4 @@ variable "private_dns_zone_resource_group_name" {
   type        = string
   default     = ""
   description = "Resource group of private DNS zone for ACR"
-
-  validation {
-    condition     = var.use_existing_private_dns_zone == true && var.private_dns_zone_resource_group_name != ""
-    error_message = "private_dns_zone_resource_group_name must be specified when use_existing_private_dns_zone is true"
-  }
 }
