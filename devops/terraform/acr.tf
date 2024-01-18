90b114f38e1560ea5a3622a92a85c12b26ce0e88
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-18 03:28:30 +0000

    Remove unused resources related to private DNS zone and private endpoint

--- devops/terraform/acr.tf
+++ devops/terraform/acr.tf
@@ -37,40 +37,40 @@ EOF
   }
 }
 
-resource "azurerm_private_dns_zone" "azurecr" {
-  count               = var.use_existing_private_dns_zone == true ? 0 : 1
-  name                = module.terraform_azurerm_environment_configuration.private_links["privatelink.azurecr.io"]
-  resource_group_name = azurerm_resource_group.mgmt.name
-  tags                = azurerm_resource_group.mgmt.tags
-  lifecycle { ignore_changes = [tags] }
-}
+# resource "azurerm_private_dns_zone" "azurecr" {
+#   count               = var.use_existing_private_dns_zone == true ? 0 : 1
+#   name                = module.terraform_azurerm_environment_configuration.private_links["privatelink.azurecr.io"]
+#   resource_group_name = azurerm_resource_group.mgmt.name
+#   tags                = azurerm_resource_group.mgmt.tags
+#   lifecycle { ignore_changes = [tags] }
+# }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "acrlink" {
-  name                  = "acrmgmtlink"
-  resource_group_name   = var.use_existing_private_dns_zone == true ? one(data.azurerm_private_dns_zone.azurecr[*].resource_group_name) : one(azurerm_private_dns_zone.azurecr[*].resource_group_name)
-  private_dns_zone_name = module.terraform_azurerm_environment_configuration.private_links["privatelink.azurecr.io"]
-  virtual_network_id    = regex("(?P<virtual_network_id>.*)\\/subnets\\/(?P<subnet>.*)", var.mgmt_acr_private_endpoint_subnet_id).virtual_network_id
-  tags                  = azurerm_resource_group.mgmt.tags
-  lifecycle { ignore_changes = [tags] }
-}
+# resource "azurerm_private_dns_zone_virtual_network_link" "acrlink" {
+#   name                  = "acrmgmtlink"
+#   resource_group_name   = var.use_existing_private_dns_zone == true ? one(data.azurerm_private_dns_zone.azurecr[*].resource_group_name) : one(azurerm_private_dns_zone.azurecr[*].resource_group_name)
+#   private_dns_zone_name = module.terraform_azurerm_environment_configuration.private_links["privatelink.azurecr.io"]
+#   virtual_network_id    = regex("(?P<virtual_network_id>.*)\\/subnets\\/(?P<subnet>.*)", var.mgmt_acr_private_endpoint_subnet_id).virtual_network_id
+#   tags                  = azurerm_resource_group.mgmt.tags
+#   lifecycle { ignore_changes = [tags] }
+# }
 
-resource "azurerm_private_endpoint" "mgmt_acr_pe" {
-  name                = "pe-mgmt-acr-devops-agents"
-  location            = azurerm_container_registry.shared_acr.location
-  resource_group_name = azurerm_container_registry.shared_acr.resource_group_name
-  subnet_id           = var.mgmt_acr_private_endpoint_subnet_id
-  tags                = azurerm_resource_group.mgmt.tags
-  lifecycle { ignore_changes = [tags] }
+# resource "azurerm_private_endpoint" "mgmt_acr_pe" {
+#   name                = "pe-mgmt-acr-devops-agents"
+#   location            = azurerm_container_registry.shared_acr.location
+#   resource_group_name = azurerm_container_registry.shared_acr.resource_group_name
+#   subnet_id           = var.mgmt_acr_private_endpoint_subnet_id
+#   tags                = azurerm_resource_group.mgmt.tags
+#   lifecycle { ignore_changes = [tags] }
 
-  private_dns_zone_group {
-    name                 = "private-dns-zone-group-mgmtacr"
-    private_dns_zone_ids = [var.use_existing_private_dns_zone == true ? one(data.azurerm_private_dns_zone.azurecr[*].id) : one(azurerm_private_dns_zone.azurecr[*].id)]
-  }
+#   private_dns_zone_group {
+#     name                 = "private-dns-zone-group-mgmtacr"
+#     private_dns_zone_ids = [var.use_existing_private_dns_zone == true ? one(data.azurerm_private_dns_zone.azurecr[*].id) : one(azurerm_private_dns_zone.azurecr[*].id)]
+#   }
 
-  private_service_connection {
-    name                           = "psc-acr-mgmt"
-    private_connection_resource_id = azurerm_container_registry.shared_acr.id
-    is_manual_connection           = false
-    subresource_names              = ["registry"]
-  }
-}
+#   private_service_connection {
+#     name                           = "psc-acr-mgmt"
+#     private_connection_resource_id = azurerm_container_registry.shared_acr.id
+#     is_manual_connection           = false
+#     subresource_names              = ["registry"]
+#   }
+# }
