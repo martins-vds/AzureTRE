b1119df2246e005447d2b428c3393712c97bd7c1
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-18 02:54:55 +0000

    Update resource_group_name variable in dns_zones module

--- core/terraform/network/main.tf
+++ core/terraform/network/main.tf
@@ -11,7 +11,7 @@ terraform {
 module "dns_zones" {
   source              = "./dns_zones"
   create_dns_zones    = var.use_existing_private_dns_zone ? false : true
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.private_dns_zone_resource_group_name
   tre_id              = var.tre_id
   arm_environment     = var.arm_environment
 }
