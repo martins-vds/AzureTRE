55918a51d0f92440f6f7b7adc95b0f9546f0b9a2
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-19 20:50:34 +0000

    Ignore changes to tags in AzureTRE database resource

--- core/terraform/statestore.tf
+++ core/terraform/statestore.tf
@@ -40,6 +40,8 @@ resource "azurerm_cosmosdb_sql_database" "tre_db" {
   name                = "AzureTRE"
   resource_group_name = azurerm_resource_group.core.name
   account_name        = azurerm_cosmosdb_account.tre_db_account.name
+
+  lifecycle { ignore_changes = [tags] }
 }
 
 moved {
