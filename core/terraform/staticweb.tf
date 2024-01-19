05f598aaafc823eca2700220b93dbd3986d0e1a4
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-19 01:58:05 +0000

    Update principal_id in azurerm_role_assignment

--- core/terraform/staticweb.tf
+++ core/terraform/staticweb.tf
@@ -29,7 +29,7 @@ resource "azurerm_storage_account" "staticweb" {
 resource "azurerm_role_assignment" "stgwriter" {
   scope                = azurerm_storage_account.staticweb.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azurerm_client_config.deployer.object_id
+  principal_id         = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_private_endpoint" "webpe" {
