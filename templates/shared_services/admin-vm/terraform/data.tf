e19fc5144c322dfe27075a8de0e043e83865be88
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-24 04:31:51 +0000

    Update version in porter.yaml and remove duplicate key vault data.tf

--- templates/shared_services/admin-vm/terraform/data.tf
+++ templates/shared_services/admin-vm/terraform/data.tf
@@ -3,6 +3,7 @@ data "azurerm_subnet" "shared" {
   virtual_network_name = local.core_vnet
   name                 = "SharedSubnet"
 }
+
 data "azurerm_key_vault" "keyvault" {
   name                = local.keyvault_name
   resource_group_name = local.core_resource_group_name
@@ -15,11 +16,6 @@ data "azurerm_resource_group" "rg" {
 data "azurerm_log_analytics_workspace" "oms-workspace" {
   name = local.law_name
   resource_group_name = data.azurerm_resource_group.rg.name
-}
-
-data "azurerm_key_vault" "keyvault" {
-  name                = local.keyvault_name
-  resource_group_name = local.core_resource_group_name
 }
 
 data "azurerm_virtual_desktop_host_pool" "core_hostpool" {
