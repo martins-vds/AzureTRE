1f89146918ebc22b1a2f765f6ac3e3d5fbc0c461
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-24 21:09:34 +0000

    Update AVD locals.tf and version.txt
    This commit updates the `rfc3339` value in `locals.tf` from "720h" to "480h" and updates the `__version__` in `version.txt` from "0.10.19" to "0.10.20".

--- core/terraform/avd/locals.tf
+++ core/terraform/avd/locals.tf
@@ -6,7 +6,7 @@ locals {
 
   workspace = "core-avd-ws-${var.tre_id}"
   hostpool  = "core-avd-hp-${var.tre_id}"
-  rfc3339   = timeadd(timestamp(), "720h")
+  rfc3339   = timeadd(timestamp(), "480h")
   appgroup  = "core-avd-ag-${var.tre_id}"
   prefix    = "core-avd-${var.tre_id}"
 }
