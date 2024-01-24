147293ef74f75f86446fb2bb574675115439011c
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-24 02:15:04 +0000

    Add AVD workspace and update AVD DAG
    This commit adds a new resource for the AVD workspace and updates the AVD DAG to use the correct host pool ID.

--- templates/workspaces/base/terraform/avd/avd.tf
+++ templates/workspaces/base/terraform/avd/avd.tf
@@ -1,21 +1,21 @@
-# Create AVD DAG
-resource "azurerm_virtual_desktop_application_group" "dag" {
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  host_pool_id        = data.azurerm_virtual_desktop_host_pool.hostpool.id
-  type                = "Desktop"
-  name                = "avg-dag-${local.workspace_resource_name_suffix}"
-  friendly_name       = "Workspace ${local.short_workspace_id} Desktop Application Group"
-  description         = "AVD application group"
-  depends_on          = [azurerm_virtual_desktop_host_pool.hostpool, azurerm_virtual_desktop_workspace.workspace]
-}
-
 resource "azurerm_virtual_desktop_workspace" "workspace" {
   name                = local.avd_workspace
   resource_group_name = var.resource_group_name
   location            = var.location
   friendly_name       = "${local.workspace_resource_name_suffix} AVD Workspace"
   description         = "${local.workspace_resource_name_suffix} AVD Workspace"
+}
+
+# Create AVD DAG
+resource "azurerm_virtual_desktop_application_group" "dag" {
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  host_pool_id        = data.azurerm_virtual_desktop_host_pool.core_hostpool.id
+  type                = "Desktop"
+  name                = "avg-dag-${local.workspace_resource_name_suffix}"
+  friendly_name       = "Workspace ${local.short_workspace_id} Desktop Application Group"
+  description         = "AVD application group"
+  depends_on          = [azurerm_virtual_desktop_workspace.workspace]
 }
 
 # Associate Workspace and DAG
