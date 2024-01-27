e33caa6640306a611eef1079654768cb6c976388
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-01-27 18:45:43 +0000

    Update private DNS zone virtual network links count

--- core/terraform/network/dns_zone_links_primary.tf
+++ core/terraform/network/dns_zone_links_primary.tf
@@ -1,5 +1,5 @@
 resource "azurerm_private_dns_zone_virtual_network_link" "azure_monitor" {
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   provider              = azurerm.primary
   name                  = "azure-monitor-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
@@ -13,7 +13,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "azure_monitor" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "azure_monitor_oms_opinsights" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "azure-monitor-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   virtual_network_id    = azurerm_virtual_network.core.id
@@ -26,7 +26,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "azure_monitor_oms_opin
 
 resource "azurerm_private_dns_zone_virtual_network_link" "azure_monitor_ods_opinsights" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "azure-monitor-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   virtual_network_id    = azurerm_virtual_network.core.id
@@ -38,7 +38,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "azure_monitor_ods_opin
 
 resource "azurerm_private_dns_zone_virtual_network_link" "azure_monitor_agentsvc" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "azure-monitor-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   virtual_network_id    = azurerm_virtual_network.core.id
@@ -50,7 +50,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "azure_monitor_agentsvc
 
 resource "azurerm_private_dns_zone_virtual_network_link" "blobcore" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "blobcore-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.blobcore.name
@@ -61,7 +61,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "blobcore" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "azurewebsites" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "azurewebsites-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   virtual_network_id    = azurerm_virtual_network.core.id
@@ -73,7 +73,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "azurewebsites" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "webcorelink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "staticwebcore-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.static_web.name
@@ -84,7 +84,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "webcorelink" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "filecorelink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "filecore-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.filecore.name
@@ -95,7 +95,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "filecorelink" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vaultcore" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "vaultcore-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.vaultcore.name
@@ -106,7 +106,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vaultcore" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "acrlink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "acrcore-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.azurecr.name
@@ -117,7 +117,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "acrlink" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "eventgridlink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "eventgrid-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.eventgrid.name
@@ -129,7 +129,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "eventgridlink" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "tablelink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "table-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.table.name
@@ -141,7 +141,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "tablelink" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "queuelink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "queue-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.queue.name
@@ -153,7 +153,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "queuelink" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "mysqllink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "mysql-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.mysql.name
@@ -165,7 +165,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "mysqllink" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "servicebuslink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "servicebus-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.servicebus.name
@@ -177,7 +177,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "servicebuslink" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "cosmoslink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "cosmos-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.cosmos.name
@@ -189,7 +189,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "cosmoslink" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "monglink" {
   provider              = azurerm.primary
-  count                 = var.use_core_private_dns_zones ? 0 : 1
+  count                 = var.use_core_private_dns_zones ? 1 : 0
   name                  = "mongo-link"
   resource_group_name   = var.private_dns_zones_resource_group_name
   private_dns_zone_name = module.dns_zones.mongo.name
