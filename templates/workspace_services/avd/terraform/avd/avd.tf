ce88e1ab9f9fced626e62ef08ef2be6aadbdc4bf
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2024-03-19 16:24:00 +0000

    add scaling plan to avd

--- templates/workspace_services/avd/terraform/avd/avd.tf
+++ templates/workspace_services/avd/terraform/avd/avd.tf
@@ -19,7 +19,39 @@ resource "azurerm_virtual_desktop_host_pool" "hostpool" {
   type                     = "Pooled"
   maximum_sessions_allowed = 16
   load_balancer_type       = "DepthFirst" #[BreadthFirst DepthFirst]
+}
 
+resource "azurerm_virtual_desktop_scaling_plan" "hostpool-scaling-plan" {
+  name                = "${local.hostpool}-scaling-plan"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  friendly_name       = "${local.hostpool}-scaling-plan"
+  description         = "${local.hostpool}-scaling-plan"
+  time_zone           = "Eastern Standard Time"
+  schedule {
+    name                                 = "Weekdays"
+    days_of_week                         = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    ramp_up_start_time                   = "05:00"
+    ramp_up_load_balancing_algorithm     = "BreadthFirst"
+    ramp_up_minimum_hosts_percent        = 20
+    ramp_up_capacity_threshold_percent   = 10
+    peak_start_time                      = "09:00"
+    peak_load_balancing_algorithm        = "BreadthFirst"
+    ramp_down_start_time                 = "19:00"
+    ramp_down_load_balancing_algorithm   = "DepthFirst"
+    ramp_down_minimum_hosts_percent      = 10
+    ramp_down_force_logoff_users         = false
+    ramp_down_wait_time_minutes          = 45
+    ramp_down_notification_message       = "Please log off in the next 45 minutes..."
+    ramp_down_capacity_threshold_percent = 5
+    ramp_down_stop_hosts_when            = "ZeroSessions"
+    off_peak_start_time                  = "22:00"
+    off_peak_load_balancing_algorithm    = "DepthFirst"
+  }
+  host_pool {
+    hostpool_id          = azurerm_virtual_desktop_host_pool.hostpool.id
+    scaling_plan_enabled = true
+  }
 }
 
 resource "azurerm_virtual_desktop_host_pool_registration_info" "registrationinfo" {
